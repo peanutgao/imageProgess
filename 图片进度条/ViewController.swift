810639bcb74eb04641114be9862712a78e4f85e3
Joseph_Gao <joseph0750@gmail.com>
2015-07-10 21:17:25 +0800

    Initial Commit

---
+++ 图片进度条/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  图片进度条
+//
+//  Created by Joseph on 7/10/15.
+//  Copyright © 2015 Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
